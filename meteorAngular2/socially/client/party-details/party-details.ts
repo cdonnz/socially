a852474eee28f23fa08d20cb7b3736c95ad761f4
Donnelly <christopher.donnelly@priceline.com>
2016-01-02 20:42:34 -0500

    chore(setup)

---
+++ meteorAngular2/socially/client/party-details/party-details.ts
@@ -0,0 +1,37 @@
+/// <reference path="../../typings/angular2-meteor.d.ts" />
+ 
+import {Component, View} from 'angular2/core';
+ 
+import {RouteParams} from 'angular2/router';
+
+import {Parties} from 'collections/parties';
+
+import {FORM_DIRECTIVES} from 'angular2/common';
+
+import {RouterLink} from 'angular2/router';
+
+@Component({
+  selector: 'party-details'
+})
+
+@View({
+  templateUrl: '/client/party-details/party-details.html',
+  directives: [RouterLink, FORM_DIRECTIVES]
+})
+
+export class PartyDetails {
+    party: Object;
+    constructor(params: RouteParams) {
+      var partyId = params.get('partyId');
+      this.party = Parties.findOne(partyId);
+    }
+    saveParty(party) {
+      Parties.update(party._id, {
+        $set: {
+          name: party.name,
+          description: party.description,
+          location: party.location
+        }
+      });
+    }    
+}
